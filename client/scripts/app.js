b9a27b6f5f538079c01d1c19768c7313e4a56487
Paulo Diniz <pdiniz_100@yahoo.com>
2015-02-16 19:40:24 -0800

    Modified code to prevent intrusion

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,20 +1,66 @@
 $(document).on('ready', function () {
+  var tagBody = '(?:[^"\'>]|"[^"]*"|\'[^\']*\')*';
+
+  var tagOrComment = new RegExp(
+    '<(?:'
+      // Comment body.
+    + '!--(?:(?:-*[^->])*--+|-?)'
+      // Special "raw text" elements whose content should be elided.
+    + '|script\\b' + tagBody + '>[\\s\\S]*?</script\\s*'
+    + '|style\\b' + tagBody + '>[\\s\\S]*?</style\\s*'
+      // Regular name
+    + '|/?[a-z]'
+    + tagBody
+    + ')>',
+    'gi');
+  function removeTags(html) {
+    var oldHtml;
+    do {
+      oldHtml = html;
+      html = html.replace(tagOrComment, '');
+    } while (html !== oldHtml);
+    return html.replace(/</g, '&lt;');
+  }
+
+
   var friends = {};
   var update = function (response) {
     console.log(response);
     var div = $('<div></div>');
     for(var i = 0, count = response.results.length; i < count; i++){
+      var text;
+      if (response.results[i].text !== undefined && response.results[i].text !== null) {
+        text = removeTags(response.results[i].text);
+      }
+      else{
+        text = ""
+      }
+      var createdAt = removeTags(response.results[i].createdAt);
+      var roomName;
+      if (response.results[i].roomname !== undefined && response.results[i].roomname !== null){
+        roomName = removeTags(response.results[i].roomname);
+      }
+      else{
+        roomName = "unknown";
+      }
+      var userName;
+      if (response.results[i].username !== undefined && response.results[i].username !== null){
+        userName = removeTags(response.results[i].username);
+      }
+      else{
+        userName = undefined;
+      }
 
       var content = $('<p></p>');
-      if (friends[response.results[i].username] === undefined){
+      if (friends[userName] === undefined){
         content.append('Username: ' + '<div class="clickMe">' + response.results[i].username + '</div>' + '<br>');
       }
       else{
         content.append('Username: ' + '<strong>' + response.results[i].username + '</strong>' + '<br>');
       }
-      content.append('Text: ' + response.results[i].text + '<br>');
-      content.append('Create At: ' + response.results[i].createdAt+ '<br>');
-      content.append('Room Name: ' + response.results[i].roomname + '<br>');
+      content.append('Text: ' + text + '<br>');
+      content.append('Create At: ' + createdAt+ '<br>');
+      content.append('Room Name: ' + roomName + '<br>');
       div.append(content);
     }
     $('.content').html(div);
@@ -22,7 +68,12 @@ $(document).on('ready', function () {
 
   $('body').on('click', '.clickMe', function(event){
     event.preventDefault();
-    friends[$(this).text()] = $(this).text()
+    if (friends[$(this).text()] === undefined){
+      console.log($(this).text());
+      friends[$(this).text()] = $(this).text();
+      $('#friends').append("<li>" + $(this).text() + "</li>");
+    }
+
   });
 
   $('body').on('submit', '.name', function (event) {
@@ -35,7 +86,7 @@ $(document).on('ready', function () {
     $('.currentRoom').text($('#roomname').val());
   });
 
-  $('body').on('click', 'button', function () {
+  $('body').on('click', '.refresh', function () {
 
     var roomName = $('.currentRoom').text();
     $.ajax('https://api.parse.com/1/classes/chatterbox', {
@@ -86,10 +137,6 @@ $(document).on('ready', function () {
 
   /// END OF DOCUMENT REA
 });
-
-
-
-
 
 
 
